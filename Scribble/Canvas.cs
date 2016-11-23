478cfe85effbfe52400d975034b25dfee980f0f3
RubyBe <benavente@outlook.com>
2016-11-22 19:39:26 -0800

    add a list of lists

--- Scribble/Canvas.cs
+++ Scribble/Canvas.cs
@@ -15,10 +15,14 @@ namespace Scribble
     {
         // A list to store a stroke as a list of points
         List<PointWithAttributes> stroke = new List<PointWithAttributes>();
+        // A list to store delete points for a redo operations
+        List<PointWithAttributes> redo = new List<PointWithAttributes>();
         // A list to store a drawing as a list of strokes
         List<List<PointWithAttributes>> strokes = new List<List<PointWithAttributes>>();
-        // A list to store delete points for a redo operations
-        List<PointWithAttributes> Redo = new List<PointWithAttributes>();
+        // A list to store a drawing as a list of strokes for redo operations
+        List<List<PointWithAttributes>> redos = new List<List<PointWithAttributes>>();
+        // A temp list to store a stroke
+        List<PointWithAttributes> tempStroke = new List<PointWithAttributes>();
         // A variable to hold the color to use
         Color c = Color.Black;
 
@@ -43,22 +47,15 @@ namespace Scribble
 
         private void Canvas_Paint(object sender, PaintEventArgs e)
         {
-            //foreach (PointWithAttributes pt in stroke)
+            //foreach (List<PointWithAttributes> stroke in strokes)
             //{
-            //    Brush br = new SolidBrush(pt.color);
-            //    e.Graphics.FillRectangle(br, pt.point.X, pt.point.Y, 10, 10);
-            //    e.Graphics.DrawRectangle(Pens.Blue, pt.point.X, pt.point.Y, 10, 10);
-            //}
-            foreach (List<PointWithAttributes> stroke in strokes)
-            {
                 foreach (PointWithAttributes pt in stroke)
                 {
                     Brush br = new SolidBrush(pt.color);
                     e.Graphics.FillRectangle(br, pt.point.X, pt.point.Y, 10, 10);
                     e.Graphics.DrawRectangle(Pens.Blue, pt.point.X, pt.point.Y, 10, 10);
                 }
-            }
-
+            //}
         }
 
         private void Canvas_KeyDown(object sender, KeyEventArgs e)
@@ -71,28 +68,33 @@ namespace Scribble
             {
                 c = Color.AliceBlue;
             }
-            if (e.KeyCode == Keys.Z && e.Control && stroke.Count > 0) // redo
+            if (e.KeyCode == Keys.Z && e.Control && strokes.Count > 0) // redo
             {
-                Redo.Add(stroke[stroke.Count - 1]);
+                redos.Add(strokes[strokes.Count - 1]);
                 strokes.RemoveAt(strokes.Count - 1);
                 Invalidate();
             }
-            if (e.KeyCode == Keys.Y && e.Control && stroke.Count > 0) // redo
+            if (e.KeyCode == Keys.Y && e.Control && strokes.Count > 0) // redo
             {
-                stroke.Add(Redo[Redo.Count - 1]);
-                Redo.RemoveAt(Redo.Count - 1);
+                strokes.Add(redos[redos.Count - 1]);
+                redos.RemoveAt(redos.Count - 1);
                 Invalidate();
             }
         }
         // clear the previous stroke in preparation for creating a new stroke
         private void Canvas_MouseDown(object sender, MouseEventArgs e)
         {
-            //stroke.Clear();
+            
         }
         // after creating a new stroke, add that stroke to a list of strokes
         private void Canvas_MouseUp(object sender, MouseEventArgs e)
         {
-            strokes.Add(stroke);
+
+            // tempStroke.AddRange(stroke);
+            // this will insert to the front of the list
+            List<PointWithAttributes> tempStroke = new List<PointWithAttributes>(stroke);
+            strokes.Add(tempStroke);
+            //  tempStroke.Clear(); // this statement clears strokes
         }
     }
 }
